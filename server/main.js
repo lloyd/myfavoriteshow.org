826a0d1dfcf20bfc7d4d1b620d65a3dd9cc768c4
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-11-16 11:54:33 -0700

    do verification

--- server/main.js
+++ server/main.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 
-// require libraries that we depend on 
+// require libraries that we depend on
 const
 express = require('express'),
-sessions = require('connect-cookie-session'),
 path = require('path'),
 postprocess = require('postprocess'),
-https = require('https'),
-querystring = require('querystring'),
 db = require('./db.js'),
-url = require('url');
+url = require('url'),
+browseridClient = require('./browserid_client.js');
 
 // the key with which session cookies are encrypted
-const COOKIE_SECRET = process.env.SEKRET || 'you love, i love, we all love beer!';
+const COOKIE_SECRET = process.env.SEKRET || 'you love, i love, we all love tv shows!';
 
 // The IP Address to listen on.
 const IP_ADDRESS = process.env.IP_ADDRESS || '127.0.0.1';
@@ -34,39 +32,17 @@ var havePersistence;
 // do some logging
 app.use(express.logger({ format: 'dev' }));
 
-// parse cookies
-app.use(express.cookieParser());
-
 // parse post bodies
 app.use(express.bodyParser());
 
-// session support using signed cookies
-app.use(function (req, res, next) {
-  if (/^\/api/.test(req.url)) {
-    return sessions({
-      secret: COOKIE_SECRET,
-      key: 'myfavoritebeer_session',
-      cookie: {
-        path: '/api',
-        httpOnly: true,
-        // when you're logged in, you're logged in for an hour
-        maxAge: (1 * 60 * 60 * 1000), 
-        secure: false
-      }
-    })(req, res, next);
-  } else {
-    return next();
-  }
-});
-
 // The next three functions contain some fancy logic to make it so
-// we can run multiple different versions of myfavoritebeer on the
+// we can run multiple different versions of myfavoriteshow on the
 // same server, each which uses a different browserid server
 // (dev/beta/prod):
 function determineEnvironment(req) {
-  if (req.headers['host'] === 'myfavoritebeer.org') return 'prod';
-  else if (req.headers['host'] === 'beta.myfavoritebeer.org') return 'beta';
-  else if (req.headers['host'] === 'dev.myfavoritebeer.org') return 'dev';
+  if (req.headers['host'] === 'myfavoriteshow.org') return 'prod';
+  else if (req.headers['host'] === 'beta.myfavoriteshow.org') return 'beta';
+  else if (req.headers['host'] === 'dev.myfavoriteshow.org') return 'dev';
   else return 'local';
 }
 
@@ -82,146 +58,24 @@ function determineBrowserIDURL(req) {
   })[determineEnvironment(req)];
 }
 
-function determineBrowserIDHost(req) {
-  return determineBrowserIDURL(req).replace(/http(s?):\/\//, '');
-}
-
 // a substitution middleware allows us to easily point at different browserid servers
 app.use(postprocess.middleware(function(req, body) {
   var browseridURL = determineBrowserIDURL(req);
+  console.log("sub in:", browseridURL);
   return body.toString().replace(new RegExp("https://browserid.org", 'g'), browseridURL);
 }));
 
-
-// /api/whoami is an API that returns the authentication status of the current session.
-// it returns a JSON encoded string containing the currently authenticated user's email
-// if someone is logged in, otherwise it returns null.
-app.get("/api/whoami", function (req, res) {
-  if (req.session && typeof req.session.email === 'string') return res.json(req.session.email);
-  return res.json(null);
-});
-
-
-// /api/login is an API which authenticates the current session.  The client includes 
-// an assertion in the post body (returned by browserid's navigator.id.getVerifiedEmail()).
-// if the assertion is valid an (encrypted) cookie is set to start the user's session.
-// returns a json encoded email if the session is successfully authenticated, otherwise
-// null.
-app.post("/api/login", function (req, res) {
-  // To verify the assertion we initiate a POST request to the browserid verifier service.
-  // If we didn't want to rely on this service, it's possible to implement verification
-  // in a library and to do it ourselves.  
-  var vreq = https.request({
-    host: determineBrowserIDHost(req),
-    path: "/verify",
-    method: 'POST'
-  }, function(vres) {
-    var body = "";
-    vres.on('data', function(chunk) { body+=chunk; } )
-        .on('end', function() {
-          try {
-            var verifierResp = JSON.parse(body);
-            var valid = verifierResp && verifierResp.status === "okay";
-            var email = valid ? verifierResp.email : null;
-            req.session.email = email;
-            if (valid) {
-              console.log("assertion verified successfully for email:", email);
-            } else {
-              console.log("failed to verify assertion:", verifierResp.reason);
-            }                
-            res.json(email);
-          } catch(e) {
-            console.log("non-JSON response from verifier");
-            // bogus response from verifier!  return null
-            res.json(null);
-          }
-        });
-  });
-  vreq.setHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  // An "audience" argument is embedded in the assertion and must match our hostname.
-  // Because this one server runs on multiple different domain names we just use
-  // the host parameter out of the request.
-  var audience = req.headers['host'] ? req.headers['host'] : localHostname;   
-  var data = querystring.stringify({
+app.post("/api/register", function (req, res) {
+  browseridClient.verify({
     assertion: req.body.assertion,
-    audience: audience
-  });
-  vreq.setHeader('Content-Length', data.length);
-  vreq.write(data);
-  vreq.end();
-  console.log("verifying assertion!");
-});
-
-// /api/logout clears the session cookie, effectively terminating the current session.
-app.post("/api/logout", function (req, res) {
-  req.session.email = null;
-  res.json(true);
-});
-
-// /api/get requires an authenticated session, and accesses the current user's favorite
-// beer out of the database.
-app.get("/api/get", function (req, res) {
-  var email;
-
-  if (req.session && typeof req.session.email === 'string') email = req.session.email;
-
-  if (!email) {
-    res.writeHead(400, {"Content-Type": "text/plain"});
-    res.write("Bad Request: you must be authenticated to get your beer");
-    res.end();
-    return;
-  }
-
-  if (!havePersistence) {
-    console.log("WARNING: get is a no-op!  we have no database configured");
-    return res.json("no database");
-  }
-
-  db.get(determineEnvironment(req), email, function(err, beer) {
+    audience: req.headers['host'] ? req.headers['host'] : localHostname,
+    verifier_url: determineBrowserIDURL(req)
+  }, function(err, email) {
     if (err) {
-      console.log("error getting beer for", email); 
-      res.writeHead(500);
+      res.writeHead(401);
       res.end();
     } else {
-      res.json(beer);
-    }
-  });
-});
-
-// /api/set requires an authenticated session, and sets the current user's favorite
-// beer in the database.
-app.post("/api/set", function (req, res) {
-  var email = req.session.email;
-
-  if (!email) {
-    res.writeHead(400, {"Content-Type": "text/plain"});
-    res.write("Bad Request: you must be authenticated to get your beer");
-    res.end();
-    return;
-  }
-
-  var beer = req.body.beer;
-
-  if (!beer) {
-    res.writeHead(400, {"Content-Type": "text/plain"});
-    res.write("Bad Request: a 'beer' parameter is required to set your favorite beer");
-    res.end();
-    return;
-  }
-
-  if (!havePersistence) {
-    console.log("WARNING: set is a no-op!  we have no database configured");
-    return res.json(true);
-  }
-
-  db.set(determineEnvironment(req), email, beer, function(err) {
-    if (err) {
-      console.log("setting beer for", email, "to", beer); 
-      res.writeHead(500);
-      res.end();
-    } else {
-      res.json(true);
+      res.json({success: true, email: email});
     }
   });
 });
--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,21 @@
-$(document).ready(function() {
-  // register a click handler for our button
-  $('.mailing_list input').keypress(function() { 
-    $('.mailing_list > button').removeAttr("disabled");
-    $('.mailing_list input').unbind();
-  });
+function failedToComplete() {
+  setupForm();
+  $(".mailing_list .sad_failure").show();
+}
 
-  $(".mailing_list button").click(function() {
+function greatSuccess(email) {
+  $(".mailing_list *").unbind();
+  $(".mailing_list > div, .mailing_list > input, .mailing_list > button").hide();
+  $(".mailing_list div.done span.email").text(email);
+  $(".mailing_list > div.done").show();
+}
+
+function setupForm() {
+  $(".mailing_list *").unbind();
+  $(".mailing_list > button").show();
+  $(".mailing_list > div").hide();
+
+  $(".mailing_list > button").click(function() {
     $('.mailing_list input').attr("disabled", true);
     $('.mailing_list > button').fadeOut(200, function() {
       $('.mailing_list .next_step').fadeIn(500, function() {
@@ -13,10 +23,36 @@ $(document).ready(function() {
           $('.mailing_list .next_step').hide();
           $('.mailing_list .waiting').show();
           navigator.id.get(function(assertion) {
-            alert("got assertion " + assertion);
-          }, { requiredEmail: $.trim($('.mailing_list input').val()) });
+            $.ajax({
+              type: 'POST',
+              url: '/api/register',
+              data: { assertion: assertion },
+              success: function(res, status, xhr) {
+                if (res.success && res.email) {
+                  greatSuccess(res.email);
+                } else {
+                  failedToComplete();
+                }
+              },
+              error: function(res, status, xhr) {
+                failedToComplete();
+              }
+            });
+          }, {
+            requiredEmail: $.trim($('.mailing_list input').val())
+          });
         });
       });
     });
   });
+
+  // register a click handler for our button
+  $('.mailing_list input').keypress(function() {
+    $('.mailing_list > button').removeAttr("disabled");
+    $('.mailing_list input').unbind();
+  });
+}
+
+$(document).ready(function() {
+  setupForm();
 });
