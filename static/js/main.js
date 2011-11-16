592d23839d82517dee55fae08c807a96ae958cd3
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-11-16 09:51:47 -0700

    rework flow a bit - signup is now next, text link is now a button

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   // register a click handler for our button
+  $('.mailing_list input').keypress(function() { 
+    $('.mailing_list > button').removeAttr("disabled");
+    $('.mailing_list input').unbind();
+  });
+
   $(".mailing_list button").click(function() {
     $('.mailing_list input').attr("disabled", true);
-    $('.mailing_list button').fadeOut(200, function() {
+    $('.mailing_list > button').fadeOut(200, function() {
       $('.mailing_list .next_step').fadeIn(500, function() {
-        $(".mailing_list a").click(function() {
+        $(".mailing_list .next_step button").click(function() {
           $('.mailing_list .next_step').hide();
           $('.mailing_list .waiting').show();
           navigator.id.get(function(assertion) {
